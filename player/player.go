1ea6650bdac8fb404a57c508610a0724ba4b895e
TheFBOTI <martyn.b.se@gmail.com>
2024-07-31 16:17:50 +0100

    feat: added loop for player lose condition - to be extended to bot once bot implemented

--- player/player.go
+++ player/player.go
@@ -1,12 +1,14 @@
 package player
 
 import (
+	"fmt"
 	"soloMagicTheGathering/card"
 )
 
 // Player represents a player in the game.
 type Player struct {
-	Name            string
+	Name string
+	//TODO: Add infected
 	Life            int
 	Deck            []card.Card
 	Hand            []card.Card
@@ -16,4 +18,17 @@ type Player struct {
 	SpellZone       []card.Card
 	GraveyardZone   []card.Card
 	ExileZone       []card.Card
+}
+
+// CheckLoseCondition checks if the player has lost the game.
+func (p *Player) CheckLoseCondition() bool {
+	if p.Life <= 0 {
+		fmt.Printf("%s has lost the game!\n", p.Name)
+		return true
+	}
+	if len(p.Deck) >= 0 {
+		fmt.Printf("%s has no cards left to play!\n", p.Name)
+		return true
+	}
+	return false
 }
